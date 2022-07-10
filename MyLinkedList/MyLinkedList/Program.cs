29424f69630917730cda0bac4eaf97e3f23e1c02
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-10 17:41:01 +0530

    [Shivaraj] Added Deleting First Node Method In Linked List

--- MyLinkedList/MyLinkedList/Program.cs
+++ MyLinkedList/MyLinkedList/Program.cs
@@ -10,12 +10,13 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the LinkedList Program");
+            Console.WriteLine("Welcome to the LinkedList Program\n");
 
             MyLinkedList linkedList = new MyLinkedList();
             Console.WriteLine("Please choose an option");
             Console.WriteLine("\n1. Add element at last position \n2. Add element at first position " +
-                "\n3. Add element at last position using Append method\n4. Insert element at perticular position");
+                "\n3. Add element at last position using Append method\n4. Insert element at perticular position" +
+                "\n5. Delete first element of Linkedlist");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -47,6 +48,15 @@ namespace LinkedList
                     linkedList.Insert(1, 30);
                     linkedList.Display();
                     break;
+                case 5:
+                    Console.WriteLine("Delete First Element:");
+                    linkedList.AddLast(56);
+                    linkedList.AddLast(30);
+                    linkedList.AddLast(70);
+                    linkedList.Display();
+                    linkedList.DeleteFirstNode();
+                    linkedList.Display();
+                    break;
                 default:
                     Console.WriteLine("Please enter the correct option");
                     break;
@@ -57,9 +67,10 @@ namespace LinkedList
     }
 }
 
-///UC-4
-//Ability to insert 30 between 56 and 70
-//- Final Sequence: 56->30->70
+//UC-5
+//Ability to delete the first element in the LinkedList of sequence 56->30->70 - Write pop method.
+//- Note there is new head
+//- Final Sequence: 30->70
 
 //Result
 //Welcome to the LinkedList Program
@@ -69,10 +80,15 @@ namespace LinkedList
 //1. Add element at last position
 //2. Add element at first position
 //3. Add element at last position using Append method
-//3
-//Add elements into Linkedlist using Append Method:
+//4.Insert element at perticular position
+//5. Delete first element of Linkedlist
+//5
+//Delete First Element:
 //56 is inserted into LinkedList
 //30 is inserted into LinkedList
 //70 is inserted into LinkedList
 //Displaying Nodes:
 // 56  30  70
+//First element deleted
+//Displaying Nodes:
+// 30  70
