0e9c13f164c25ff528e1abeb0e9931ed3f7faf5e
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-10 17:26:53 +0530

    [Shivaraj] Added Creating Linked List

--- MyLinkedList/MyLinkedList/Program.cs
+++ MyLinkedList/MyLinkedList/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Linked List Problems");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Program");
+
+            MyLinkedList linkedList = new MyLinkedList();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            linkedList.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
+//UC-1
+//Lets create a simple Linked List of 56, 30 and 70
+
+//Result
+//Welcome to the LinkedList Program
+//56 is inserted into LinkedList
+//30 is inserted into LinkedList
+//70 is inserted into LinkedList
+//Displaying Nodes:
+// 56  30  70
